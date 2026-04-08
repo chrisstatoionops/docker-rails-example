2bab5b491d0bd8cbed79cab7e0986eef36f94779
Criostóir O'Codlatáin Lachtna <chris@stationops.com>
2026-04-08 15:07:06 -0300

    delete file s3

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,9 +1,17 @@
 class Upload < ApplicationRecord
-  has_one_attached :file, dependent: :purge
+  has_one_attached :file
+
+  # Rails only auto-purges blobs when dependent: :purge_later (async). Using dependent: :purge
+  # does not purge storage. Purge synchronously so S3/local files are removed without a worker.
+  before_destroy :purge_file_from_storage
 
   validate :file_must_be_attached
 
   private
+
+  def purge_file_from_storage
+    file.purge if file.attached?
+  end
 
   def file_must_be_attached
     errors.add(:file, "must be attached") unless file.attached?
